dbb53afc247bd0092166338e26f405cb89fded9e
Andres Osante <andresosante@C02FW5G2MD6M.local>
2022-01-05 23:23:16 -0600

    .

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -10,27 +10,33 @@ export default {
     },
   },
   args: {
-    children: "Button",
+    children: "Button", // will be the default children prop if not specified in story
   },
 };
 
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
-
 const Template = (args) => <Button {...args} />;
 
-export const PrimaryA = Template.bind({});
-PrimaryA.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   variant: "primary",
-  children: "Primary Args",
+  children: "Primary Button",
 };
 
-export const SecondaryA = Template.bind({});
-SecondaryA.args = {
+export const Secondary = Template.bind({});
+Secondary.args = {
   variant: "secondary",
-  children: "Secondary Args",
+  children: "Secondary Button",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  variant: "success",
+  children: "Success Button",
+};
+export const Danger = Template.bind({});
+Danger.args = {
+  variant: "danger",
+  children: "Danger Button",
 };
 
 export const LongPrimaryA = Template.bind({});
